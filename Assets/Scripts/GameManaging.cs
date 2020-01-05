43dc3f7f781c61296fd0167632a9d792468b5a1b
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2020-01-05 11:24:45 +0000

    BIG COMMIT! Main Menu and Game Over UIs built
    - Added a basic title screen
    - Added a basic game over screen
    - Tried it out on a 'build and run' test, and the buttons are functioning properly.

---
+++ Assets/Scripts/GameManaging.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManaging : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
