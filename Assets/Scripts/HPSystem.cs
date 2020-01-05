c82210e451ffdc2bcb8202fa8d98a912bafad596
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2020-01-05 10:25:49 +0000

    LLE move towards the player
    - Added script so that a LLE will face the player at all times.
    - Could be worth implementing a script where they will only face the player when they are a certain distance away from you.

---
+++ Assets/Scripts/HPSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int> { }
+public class HPSystem : MonoBehaviour
+{
+    public int hp = 10;//set HP
+    public UnityEvent onDie;
+    public OnDamagedEvent onDamaged;
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        onDamaged.Invoke(hp);//takes dmg
+        if (hp < 1)
+        {
+            onDie.Invoke();//oof
+        }
+    }
+}
