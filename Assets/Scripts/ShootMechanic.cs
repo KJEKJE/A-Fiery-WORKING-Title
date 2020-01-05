c05193f2f548c02dc00ed2f9b5fdd5a688af4cf1
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2020-01-05 07:53:46 +0000

    Added key mechanics
    - Flare can move with arrow keys, or wasd keys now.
    - Camera moves with the player
    - Created a fireball projectile. Now the player can shoot a fireball by clicking.

---
+++ Assets/Scripts/ShootMechanic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootMechanic : MonoBehaviour
+{
+    public GameObject pewPrefab;
+    public Transform pewSpawn;
+    public float fireTime = 0.5f;
+    private bool isFiring = false;
+    private void SetFiring()
+    {
+        isFiring = false;
+    }
+    private void Fire()
+    {
+        isFiring = true;
+        Instantiate(pewPrefab, pewSpawn.position, pewSpawn.rotation);
+        if (GetComponent<AudioSource>() != null)//checks for an audio clip. see 2c for more
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Invoke("SetFiring", fireTime);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+    }
+}
